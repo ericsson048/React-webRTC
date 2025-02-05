5f717dbac5e55a322b35400a9ef9a31ecb80e073
eericsson048 <eericsson048@gmail.com>
2025-02-05 17:45:18 +0200

    Mise à jour de l'URL du serveur dans SocketProvider pour utiliser l'URL de production

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useMemo, useContext } from "react";
 import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
-const SOCKET_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:8000";
+const SOCKET_URL = process.env.REACT_APP_SERVER_URL||"https://react-webrtc-server-68bx.onrender.com" || "http://localhost:8000";
 
 
 export const useSocket = () => {
